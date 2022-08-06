075c63d3bc5c46d20f43b80d037c86406c1b6602
tnp2004 <iceandiceisice2004@gmail.com>
2022-08-06 12:13:40 +0700

    big update

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const listsBox = document.getElementById("todolistsBox")
 const btnAddList = document.getElementById("btnAddList")
 const inputAddList = document.getElementById("inputAddList")
+const menu = document.getElementById("menu")
+const clearAll = document.getElementById("clearAll")
+const done = document.getElementById("done")
+const allTasks = document.getElementById("allTasks")
 
-const TODOLISTS = []
+let TODOLISTS = []
+let currentMenu = TODOLISTS
 
 // constructor function
 function createTodoObject (title) {
@@ -14,11 +19,15 @@ function createTodoObject (title) {
 const createElementTodoList = (todo) => {
 
     const wrapperTodo = document.createElement("a")
-    wrapperTodo.classList.add("list-group-item", "list-group-item-action", "list-group-item-info", "p-3", "border")
+    wrapperTodo.classList.add("wrap-todo", "list-group-item", "list-group-item-action", "list-group-item-info", "p-3", "border", "col-12")
     wrapperTodo.setAttribute("aria-current", true)
     wrapperTodo.addEventListener("click", () => {
         wrapperTodo.classList.toggle("active")
+        todo.isDone = !todo.isDone
     })
+    if (todo.isDone) {
+        wrapperTodo.classList.add("active")
+    }
 
     const titleTodo = document.createElement("div")
     titleTodo.classList.add("d-flex", "w-100", "justify-content-between")
@@ -28,12 +37,9 @@ const createElementTodoList = (todo) => {
     heading.innerHTML = todo.title
 
     const deleteBtn = document.createElement("button")
-    deleteBtn.classList.add("btn", "btn-danger", "btn-sm")
+    deleteBtn.classList.add("delete-btn", "btn", "btn-danger", "btn-sm")
     deleteBtn.innerHTML = "Delete"
-    deleteBtn.addEventListener("click", (e) => {
-        e.stopPropagation()
-        // do some function to delete the list
-    })
+    deleteBtn.addEventListener("click", deleteTodo)
 
     const date = document.createElement("small")
     date.innerHTML = `Created ${todo.created}`
@@ -44,10 +50,30 @@ const createElementTodoList = (todo) => {
     listsBox.append (wrapperTodo)
 }
 
-const loopCreateTodoList = () => {
+const doneTasks = () => {
+    const doneTasks = TODOLISTS.filter(todo => todo.isDone)
+    return doneTasks
+}
+
+const deleteTodo = (e) => {
+    e.stopPropagation()
+    const targetToDelete = e.target.parentElement.children[0].innerHTML
     TODOLISTS.forEach(todo => {
+        if (todo.title === targetToDelete) {
+            TODOLISTS.splice(TODOLISTS.indexOf(todo), 1)
+        }
+    })
+    loopCreateTodoList(TODOLISTS)
+}
+
+const loopCreateTodoList = (tasksStorage) => {
+    clear()
+    tasksStorage.forEach(todo => {
         createElementTodoList(todo)
     })
+    if (tasksStorage.length === 0) {
+        noTodo()
+    }
 }
 
 const clear = () => {
@@ -55,12 +81,33 @@ const clear = () => {
     listsBox.innerHTML = ""
 }
 
+const noTodo = () => {
+    const noTodo = document.createElement("div")
+    noTodo.classList.add("no-todo", "text-center", "fs-2", "mt-5", "text-muted")
+    noTodo.innerHTML = "No tasks"
+    listsBox.append(noTodo)
+}
+noTodo()
+
+allTasks.addEventListener("click", () => {
+    currentMenu = TODOLISTS
+    loopCreateTodoList(currentMenu)
+})
+
+done.addEventListener("click", () => {
+    currentMenu = doneTasks()
+    loopCreateTodoList(currentMenu)
+})
+
+clearAll.addEventListener("click", () => {
+    TODOLISTS = []
+    loopCreateTodoList(TODOLISTS)
+})
 btnAddList.addEventListener("click", () => {
   if (inputAddList.value) {
     const title = inputAddList.value
     const todoObj = new createTodoObject(title)
     TODOLISTS.push(todoObj)
-    clear()
-    loopCreateTodoList()
+    loopCreateTodoList(currentMenu)
   }
 })
