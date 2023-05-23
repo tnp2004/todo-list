5929af289036ad5e2575150646ac29e37ee2b6bc
tnp2004 <iceandiceisice2004@gmail.com>
2023-05-23 15:53:40 +0700

    Set up migrate to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import '../app.css'
+</script>
+
+<div class="h-screen bg-slate-800">
+    <div>
+        <h1 class="text-2xl text-center text-white font-bold">Todo list</h1>
+        
+    </div>
+</div>
