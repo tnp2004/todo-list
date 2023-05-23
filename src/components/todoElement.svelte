ae51ad8c7d44a8eb131e067a159254fe9881b67d
tnp2004 <iceandiceisice2004@gmail.com>
2023-05-23 20:02:05 +0700

    Separate todo element to component

---
+++ src/components/todoElement.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  export let id: number;
+  export let todo: string;
+  export let done: boolean;
+
+  const dispatch = createEventDispatcher();
+
+  const toggleIsdone = () => {
+    dispatch("toggleDone", {
+      id
+    })
+  }
+
+  const removeTodo = () => {
+    dispatch("remove", {
+      id,
+    });
+  };
+</script>
+
+<button
+  on:click={toggleIsdone}
+  class="bg-slate-700 bg-opacity-80 px-2 my-1 w-full flex items-center justify-center align-middle"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    width="30"
+    zoomAndPan="magnify"
+    viewBox="0 0 30 30.000001"
+    height="30"
+    preserveAspectRatio="xMidYMid meet"
+    version="1.0"
+    ><defs
+      ><clipPath id="id1"
+        ><path
+          d="M 2.328125 4.222656 L 27.734375 4.222656 L 27.734375 24.542969 L 2.328125 24.542969 Z M 2.328125 4.222656 "
+          clip-rule="nonzero"
+        /></clipPath
+      ></defs
+    ><g clip-path="url(#id1)"
+      ><path
+        fill={done ? "#0ea5e9" : "#1e293b"}
+        d="M 27.5 7.53125 L 24.464844 4.542969 C 24.15625 4.238281 23.65625 4.238281 23.347656 4.542969 L 11.035156 16.667969 L 6.824219 12.523438 C 6.527344 12.230469 6 12.230469 5.703125 12.523438 L 2.640625 15.539062 C 2.332031 15.84375 2.332031 16.335938 2.640625 16.640625 L 10.445312 24.324219 C 10.59375 24.472656 10.796875 24.554688 11.007812 24.554688 C 11.214844 24.554688 11.417969 24.472656 11.566406 24.324219 L 27.5 8.632812 C 27.648438 8.488281 27.734375 8.289062 27.734375 8.082031 C 27.734375 7.875 27.648438 7.679688 27.5 7.53125 Z M 27.5 7.53125 "
+        fill-opacity="1"
+        fill-rule="nonzero"
+      /></g
+    ></svg
+  >
+  <h2 class="text-xl text-white font-bold w-full">{todo}</h2>
+  <button
+    on:click={removeTodo}
+    class="text-slate-800 hover:text-sky-400 font-bold px-1">X</button
+  >
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { fly } from "svelte/transition";
+  import TodoElement from "../components/todoElement.svelte";
   import "../app.css";
 
   let inputValue = "";
@@ -29,13 +29,24 @@
     inputValue = "";
   };
 
-  const removeTodo = (id: number) => {
-    todos = todos.filter((todo) => todo.id != id);
+  const toggleIsDone = (event: any) => {
+    const { id } = event.detail;
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id === id) {
+        todos[i].done = !todos[i].done
+        break
+      }
+    }
   };
 
+  const removeTodo = (event: { detail: { id: any } }) => {
+    const { id } = event.detail;
+    todos = todos.filter((todo) => todo.id != id);
+  };
 </script>
 
 <div class="h-screen bg-slate-800 flex items-center justify-center">
+  
   <div class="w-96 pt-5 drop-shadow-2xl bg-slate-900 bg-opacity-40">
     <h1 class="text-2xl text-center text-white font-bold">Todo list</h1>
     <div class="text-center my-4 mx-auto px-5">
@@ -51,6 +62,7 @@
           class="bg-slate-700 text-white font-bold p-1 rounded-r-sm">ADD</button
         >
       </div>
+
       <div
         class="flex bg-slate-800 text-slate-600 bg-opacity-70 h-10 rounded-t-xl overflow-hidden font-bold"
       >
@@ -73,52 +85,28 @@
       <div class="bg-slate-700 bg-opacity-20 rounded-b h-96 p-1">
         {#if todos.length !== 0}
           {#if filter === "all"}
-            {#each todos as { id, todo, done }, i (id)}
-              <button
-                on:click={() => todos[i].done = !todos[i].done}
-                transition:fly
-                class="bg-slate-700 bg-opacity-80 px-2 my-1 w-full flex items-center justify-center align-middle"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" zoomAndPan="magnify" viewBox="0 0 30 30.000001" height="30" preserveAspectRatio="xMidYMid meet" version="1.0"><defs><clipPath id="id1"><path d="M 2.328125 4.222656 L 27.734375 4.222656 L 27.734375 24.542969 L 2.328125 24.542969 Z M 2.328125 4.222656 " clip-rule="nonzero"/></clipPath></defs><g clip-path="url(#id1)"><path fill={done ? "#0ea5e9" : "#1e293b"} d="M 27.5 7.53125 L 24.464844 4.542969 C 24.15625 4.238281 23.65625 4.238281 23.347656 4.542969 L 11.035156 16.667969 L 6.824219 12.523438 C 6.527344 12.230469 6 12.230469 5.703125 12.523438 L 2.640625 15.539062 C 2.332031 15.84375 2.332031 16.335938 2.640625 16.640625 L 10.445312 24.324219 C 10.59375 24.472656 10.796875 24.554688 11.007812 24.554688 C 11.214844 24.554688 11.417969 24.472656 11.566406 24.324219 L 27.5 8.632812 C 27.648438 8.488281 27.734375 8.289062 27.734375 8.082031 C 27.734375 7.875 27.648438 7.679688 27.5 7.53125 Z M 27.5 7.53125 " fill-opacity="1" fill-rule="nonzero"/></g></svg>
-                <h2 class="text-xl text-white font-bold w-full">{todo}</h2>
-                <button
-                  on:click={() => removeTodo(id)}
-                  class="text-slate-800 hover:text-sky-400 font-bold px-1"
-                  >X</button
-                >
-              </button>
+            {#each todos as todo, i (todo.id)}
+              <TodoElement
+                on:remove={removeTodo}
+                on:toggleDone={toggleIsDone}
+                {...todo}
+              />
             {/each}
           {:else if filter === "todo"}
-            {#each todoNotDone as { id, todo, done }, i (id)}
-            <button
-            on:click={() => todos[i].done = !todos[i].done}
-            transition:fly
-            class="bg-slate-700 bg-opacity-80 px-2 my-1 w-full flex items-center justify-center align-middle"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" zoomAndPan="magnify" viewBox="0 0 30 30.000001" height="30" preserveAspectRatio="xMidYMid meet" version="1.0"><defs><clipPath id="id1"><path d="M 2.328125 4.222656 L 27.734375 4.222656 L 27.734375 24.542969 L 2.328125 24.542969 Z M 2.328125 4.222656 " clip-rule="nonzero"/></clipPath></defs><g clip-path="url(#id1)"><path fill={done ? "#0ea5e9" : "#1e293b"} d="M 27.5 7.53125 L 24.464844 4.542969 C 24.15625 4.238281 23.65625 4.238281 23.347656 4.542969 L 11.035156 16.667969 L 6.824219 12.523438 C 6.527344 12.230469 6 12.230469 5.703125 12.523438 L 2.640625 15.539062 C 2.332031 15.84375 2.332031 16.335938 2.640625 16.640625 L 10.445312 24.324219 C 10.59375 24.472656 10.796875 24.554688 11.007812 24.554688 C 11.214844 24.554688 11.417969 24.472656 11.566406 24.324219 L 27.5 8.632812 C 27.648438 8.488281 27.734375 8.289062 27.734375 8.082031 C 27.734375 7.875 27.648438 7.679688 27.5 7.53125 Z M 27.5 7.53125 " fill-opacity="1" fill-rule="nonzero"/></g></svg>
-            <h2 class="text-xl text-white font-bold w-full">{todo}</h2>
-            <button
-              on:click={() => removeTodo(id)}
-              class="text-slate-800 hover:text-sky-400 font-bold px-1"
-              >X</button
-            >
-          </button>
+            {#each todoNotDone as todo, i (todo.id)}
+              <TodoElement
+                on:remove={removeTodo}
+                on:toggleDone={toggleIsDone}
+                {...todo}
+              />
             {/each}
           {:else if filter === "completed"}
-            {#each todoDone as { id, todo, done }, i (id)}
-            <button
-            on:click={() => todos[i].done = !todos[i].done}
-            transition:fly
-            class="bg-slate-700 bg-opacity-80 px-2 my-1 w-full flex items-center justify-center align-middle"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" zoomAndPan="magnify" viewBox="0 0 30 30.000001" height="30" preserveAspectRatio="xMidYMid meet" version="1.0"><defs><clipPath id="id1"><path d="M 2.328125 4.222656 L 27.734375 4.222656 L 27.734375 24.542969 L 2.328125 24.542969 Z M 2.328125 4.222656 " clip-rule="nonzero"/></clipPath></defs><g clip-path="url(#id1)"><path fill={done ? "#0ea5e9" : "#1e293b"} d="M 27.5 7.53125 L 24.464844 4.542969 C 24.15625 4.238281 23.65625 4.238281 23.347656 4.542969 L 11.035156 16.667969 L 6.824219 12.523438 C 6.527344 12.230469 6 12.230469 5.703125 12.523438 L 2.640625 15.539062 C 2.332031 15.84375 2.332031 16.335938 2.640625 16.640625 L 10.445312 24.324219 C 10.59375 24.472656 10.796875 24.554688 11.007812 24.554688 C 11.214844 24.554688 11.417969 24.472656 11.566406 24.324219 L 27.5 8.632812 C 27.648438 8.488281 27.734375 8.289062 27.734375 8.082031 C 27.734375 7.875 27.648438 7.679688 27.5 7.53125 Z M 27.5 7.53125 " fill-opacity="1" fill-rule="nonzero"/></g></svg>
-            <h2 class="text-xl text-white font-bold w-full">{todo}</h2>
-            <button
-              on:click={() => removeTodo(id)}
-              class="text-slate-800 hover:text-sky-400 font-bold px-1"
-              >X</button
-            >
-          </button>
+            {#each todoDone as todo, i (todo.id)}
+              <TodoElement
+                on:remove={removeTodo}
+                on:toggleDone={toggleIsDone}
+                {...todo}
+              />
             {/each}
           {/if}
         {:else}
